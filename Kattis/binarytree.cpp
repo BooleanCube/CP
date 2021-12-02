ed42191b31c10559acc2d69ea608249a8f39aecd
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2021-12-02 15:18:34 -0500

    tle rn

--- Kattis/binarytree.cpp
+++ Kattis/binarytree.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int findIndex(string a, int idnx) {
     int idx = idnx;
-    for(int i=0; i<a.length(); i++) {
+    for(long unsigned int i=0; i<a.length(); i++) {
         char c = a.at(i);
         if(c == 'L') idx = idx*2;
         else if(c == 'R') idx = idx*2+1;
@@ -16,12 +16,27 @@ unordered_set<string> getPermutations(string t) {
     unordered_set<string> *r = new unordered_set<string>();
 	deque<string> *stack = new deque<string>();
 	stack->push_back("");
-	//while
+	r->insert("");
+	long unsigned int prevLength = 0;
+	long unsigned int s = -1;
+	for(long unsigned int i=0; i<stack->size(); i++) {
+		string prefix = stack->front();
+		stack->pop_front();
+		if(prefix.length() > prevLength) s=0;
+		else s++;
+		for(long unsigned int j=s; j<t.length(); j++) {
+			string total = prefix + t.at(j);
+			stack->push_back(total);
+			r->insert(total);
+			prevLength = prefix.length() + 1;
+		}
+	}
 	return *r;
 }
 
 int main() {
     int n; cin >> n;
+	int o = n;
     while(n-- > 0) {
         string s; cin >> s;
         string t; cin >> t;
@@ -29,6 +44,6 @@ int main() {
         unordered_set<int> *visitedIndexes = new unordered_set<int>();
         unordered_set<string> p = getPermutations(t);
         for(string a : p) visitedIndexes->insert(findIndex(a, current));
-        cout << visitedIndexes->size() << endl;
+        cout << "Case " << o-n << ": " << visitedIndexes->size() % 21092013 << endl;
     }
 }
