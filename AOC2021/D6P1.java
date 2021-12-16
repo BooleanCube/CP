c6aaf0221efa311919bdb9068cf3df47d1a18ca7
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2021-12-16 15:26:36 -0500

    Update D6P1.java

--- AOC2021/D6P1.java
+++ AOC2021/D6P1.java
@@ -8,14 +8,18 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(bf.readLine(), ",");
 		ArrayList<Integer> fishies = new ArrayList<>();
 		while(st.hasMoreTokens()) fishies.add(Integer.parseInt(st.nextToken()));
-		for(int i=0; i<80; i++) {
+		for(int i=0; i<256; i++) {
 			int countOnes = 0;
-			for(int j=0; j<fishies.size(); i++) {
-				if(fishies.get(j) == 1) countOnes++;
-				fishies.add(j, fishies.remove(j));
+			for(int j=0; j<fishies.size(); j++) {
+				if(fishies.get(j) == 0) {
+					fishies.add(j, fishies.remove(j)+6);
+					countOnes++;
+				}
+				else fishies.add(j, fishies.remove(j)-1);
 			}
 			while(countOnes-- > 0) fishies.add(8);
-      System.out.println(fishies);
+      			System.out.println(i);
+			System.out.println(fishies.size());
 		}
 		System.out.println(fishies.size());
 	}
