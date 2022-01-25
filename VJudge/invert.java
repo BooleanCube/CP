f4e6fe796176e4b485579d9a3b3de3deaec3f005
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2022-01-24 22:08:28 -0500

    invertomancer

---
+++ VJudge/invert.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class invert {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int w = Integer.parseInt(bf.readLine());
+        for(int i=0; i<w; i++) {
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            int[] world = new int[c];
+            for(int a=0; a<r; a++) {
+                char[] line = bf.readLine().toCharArray();
+                for(int b=0; b<line.length; b++) if(line[b] == '#') world[k]++;
+            }
+            boolean norm = true;
+            int counter = 0;
+            for(int j=0; j<c-1; j++) {
+                if((norm == true && world[j+1]-world[j]<k) &&
+                   (norm == false && (r-world[j+1])-(r-world[j])<k)) {
+                    norm = !norm;
+                    ++counter;
+                    --j;
+                } 
+            }
+            System.out.println("World #" + i + ": " + counter);
+        }
+    }
+}
