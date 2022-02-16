ec739ed4944b9a163b68adf5f3cb66f1b56007d9
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2022-02-16 12:28:00 -0500

    more ac

--- CSES/maxsubarraysum.cpp
+++ CSES/maxsubarraysum.cpp
@@ -1,9 +1,9 @@
-//wrong answer because minIdx cant be greater than maxIdx you bobo
+//wrong answer because it no work when they both negative you bobo
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 typedef long long ll;
-
+ 
 int main() {
 	ll min = LLONG_MAX; int minIdx = 0;
 	ll max = LLONG_MIN; int maxIdx = 0;
@@ -12,9 +12,13 @@ int main() {
 	for(int i=1; i<n+1; i++) {
 		int p; cin >> p;
 		sums[i] = sums[i-1] + p;
-		if(sums[i] < min) { min = sums[i]; minIdx = i; }
-		if(sums[i] > max) { max = sums[i]; maxIdx = i; }
+		if(sums[i] > max) { 
+			max = sums[i]; maxIdx = i;
+			for(int j=i-1; j>=0; j--) {
+				if(sums[j] < min) { min = sums[j]; minIdx = j; }
+			}
+		}
+ 
 	}
-	cout << min << " " << max << " " << minIdx << " " << maxIdx << endl;
 	cout << sums[maxIdx]-sums[minIdx] << endl;
 }
