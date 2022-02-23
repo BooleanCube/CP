f4318211fbd6f9053ddb7edead2a3de2279e4107
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2022-02-23 08:15:51 -0500

    max subarray sum

--- CSES/maxsubarraysum.cpp
+++ CSES/maxsubarraysum.cpp
@@ -1,24 +1,20 @@
-//wrong answer because it no work when they all negative you bobo
 #include <bits/stdc++.h>
 using namespace std;
  
 typedef long long ll;
  
 int main() {
-	ll min = LLONG_MAX; int minIdx = 0;
-	ll max = LLONG_MIN; int maxIdx = 0;
 	int n; cin >> n;
 	ll sums[n+1]; sums[0] = 0;
 	for(int i=1; i<n+1; i++) {
 		int p; cin >> p;
 		sums[i] = sums[i-1] + p;
-		if(sums[i] > max) { 
-			max = sums[i]; maxIdx = i;
-			for(int j=i-1; j>=0; j--) {
-				if(sums[j] < min) { min = sums[j]; minIdx = j; }
-			}
-		}
- 
 	}
-	cout << sums[maxIdx]-sums[minIdx] << endl;
+	ll maxSum = sums[1];
+	ll minPfx = sums[0];
+	for(int i=1; i<=n; i++) {
+		maxSum = max(maxSum, sums[i]-minPfx);
+		minPfx = min(minPfx, sums[i]);
+	}
+	cout << maxSum << endl;
 }
