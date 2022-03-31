5709bd9f6a3f24666e09dca3a8dfee7a38c48bc6
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2022-03-31 08:22:06 -0400

    Update subarraysums1.cpp

--- CSES/subarraysums1.cpp
+++ CSES/subarraysums1.cpp
@@ -1,20 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-
 int main() {
-	ll n,x; cin >> n >> x;
-	vector<ll> nums(n-1,0);
-	for(int i=0; i<n; i++) cin >> nums[i];
-	int l=0,r=0;
-	ll sum=0, c=0;
-	while(r<n) {
-		sum += nums[r];
-		cout << l << " " << r << " " << sum << endl;
-		if(sum > x) sum -= nums[l], l++;
-		if(sum == x) c++;
-		r++;
-	}
-	cout << c << endl;
+  int n, x;
+  cin >> n >> x;
+  int arr[n];
+  for(int i=0; i<n; i++) cin >> arr[i];
+  int sum=0, c=0; 
+  for(int left=0, right=0; right<n; right++){
+	sum += arr[right];
+	while(sum>x){
+	  sum -= arr[left];  
+	  left++;
+	}  
+	if(sum==x) c++;
+  }
+  cout << c << endl;
 }
