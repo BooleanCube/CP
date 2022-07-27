445d82a2d18f4c76ea4747755c7ce76c8e07d0b1
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2022-07-27 16:46:43 -0400

    Delete Cornell directory

--- Cornell/Warmup2021~1/aryaknow.py
+++
@@ -1,7 +0,0 @@
-a, b = map(int, input().split(" "))
-if a==b:
-	print("Depends on who starts")
-elif a<b:
-	print("Arya will think of something else")
-else:
-	print("Margaret will think of something else")
