639b3e71b1e8ae6b3cea6bba5c7b0609987a478b
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2022-07-27 16:38:55 -0400

    Delete CodeForces directory

--- CodeForces/helpfulmaths.py
+++
@@ -1,1 +0,0 @@
-l=input().split("+"); l.sort(); print("+".join(l));
--- CodeForces/watermelon.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int c; cin >> c;
-    string a = c%2==0 && c>2 ? "YES" : "NO";
-    cout << a;
-    return 0;
-}
