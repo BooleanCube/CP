4a844f05796d904eb76726fca01d0233aa3d2c12
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2022-07-27 16:47:45 -0400

    Delete codeforces directory

--- codeforces/dominopiling.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    int n, m; cin >> n >> m;
-    int k = (m*n)/2;
-    cout << k;
-}
--- codeforces/marathon.py
+++
@@ -1,4 +0,0 @@
-t = int(input())
-for i in range(t):
-    l = list(map(int,input().split()))
-    print(sum(1 for j in l if j>l[0]))
