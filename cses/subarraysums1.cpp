64a2d45fe8d82e270ecdc7fab6eef30d7c6286f4
boole <johnnyfastfigners1234@gmail.com>
2022-07-31 09:12:25 -0400

    stick lengths

--- cses/subarraysums1.cpp
+++ cses/subarraysums1.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 
 int main() {
-  int n, x;
-  cin >> n >> x;
-  int arr[n];
-  for(int i=0; i<n; i++) cin >> arr[i];
-  int sum=0, c=0; 
-  for(int left=0, right=0; right<n; right++){
-	sum += arr[right];
-	while(sum>x){
-	  sum -= arr[left];  
-	  left++;
-	}  
-	if(sum==x) c++;
-  }
-  cout << c << endl;
+    int n, x;
+    cin >> n >> x;
+    int arr[n];
+    for(int i=0; i<n; i++) cin >> arr[i];
+    int sum=0, c=0; 
+    for(int left=0, right=0; right<n; right++) {
+        sum += arr[right];
+        while(sum>x) {
+            sum -= arr[left];  
+            left++; 
+            if(sum==x) c++;
+        }
+    }
+    cout << c << endl;
 }
