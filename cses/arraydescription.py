5cf9e684ded85aa0f564325ebf3fd4e19e455938
boole <samarthupadhya2309@gmail.com>
2023-03-04 13:04:57 -0500

    cses problems

--- cses/arraydescription.py
+++ cses/arraydescription.py
@@ -1,16 +1,46 @@
 n, m = map(int, input().split())
 l = list(map(int, input().split()))
-p = 1
-if l[0] == 0 and l[1]>1: p*=3
-elif l[0] == 0: p*=2
-elif l[-1] == 0 and l[-2]<m: p*=3
-elif l[-1] == 0: p*=2
-for i in range(1, n-1):
-    if l[i] != 0: continue
-    a = l[i-1]
-    b = l[i+1]
-    d = abs(b-a)
-    if d==1: p*=2
-    elif d==0 and (a==1 or a==m): p*=2
-    elif d==0: p*=3
-print(p)
+v = [0]*n
+
+def clamp(minimum, x, maximum):
+    return max(minimum, min(x, maximum))
+
+def next(l, idx):
+    if idx > -1:
+        return set([clamp(1, l[idx]-1, m), clamp(1, l[idx], m), clamp(1, l[idx]+1, m)])
+    return list(range(1, m+1))
+
+def nextv(value):
+    return set([clamp(1, value-1, m), clamp(1, value, m), clamp(1, value+1, m)])
+
+def dfs(l, idx):
+    value = 0
+    if v[idx] or l[idx]:
+        return 1
+    stack = [(i, 0) for i in next(l, idx-1)]
+    while len(stack) > 0:
+        current = stack.pop(-1)
+        cur = current[0]
+        path = current[1]
+        if idx+path+1 >= len(l):
+            value += path+1
+            continue
+        v[idx+path] = 1
+        path += 1
+        if l[idx+path] == 0 and path == 0:
+            for i in next(l, idx-1):
+                stack.append((i, path))
+        elif l[idx+path] == 0:
+            for i in nextv(cur):
+                node = (i, path)
+                stack.append(node)
+        elif abs(cur-l[idx+path]) < 2:
+            value += 1
+    return value
+
+ans = 1
+for i in range(len(l)):
+    ans *= dfs(l, i)
+    ans %= 1000000007
+print(ans)
+
