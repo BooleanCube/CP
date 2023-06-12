d3229f406d42fcbaa8c734539c2b0bbaafca2df5
boole <samarthupadhya2309@gmail.com>
2023-06-12 16:44:00 -0400

    updated attempt

--- codeforces/ranomnumbers.py
+++ codeforces/ranomnumbers.py
@@ -7,19 +7,43 @@ for _ in range(t):
     if len(set(s))==1 and s[0] == "E":
         print(val["E"]*len(s))
         continue
-    psum = [0]*(len(s))
+    psuma = [0]*(len(s))
+    psumb = [0]*(len(s))
+    psumc = [0]*(len(s))
+    psumd = [0]*(len(s))
+    psume = [0]*(len(s))
+    psum = [psuma, psumb, psumc, psumd, psume]
+    let = "ABCDE"
     for i in range(1, len(s)):
-        if not s[i-1] == "E":
-            psum[i] = val[s[i-1]] + psum[i-1]
+        psuma[i] = val[s[i-1]] + psuma[i-1]
+        if not s[i-1] >= "B":
+            psumb[i] = val[s[i-1]] + psumb[i-1]
         else:
-            psum[i] = -val[s[i-1]] + psum[i-1]
+            psumb[i] = -val[s[i-1]] + psumb[i-1]
+        if not s[i-1] >= "C":
+            psumc[i] = val[s[i-1]] + psumc[i-1]
+        else:
+            psumc[i] = -val[s[i-1]] + psumc[i-1]
+        if not s[i-1] >= "D":
+            psumd[i] = val[s[i-1]] + psumd[i-1]
+        else:
+            psumd[i] = -val[s[i-1]] + psumd[i-1]
+        if not s[i-1] == "E":
+            psume[i] = val[s[i-1]] + psume[i-1]
+        else:
+            psume[i] = -val[s[i-1]] + psume[i-1]
     ans = 0
     b = 0
     m = []
     for i in range(len(s)-1, -1, -1):
         c = s[i]
         v = val[c]
-        m.append(10000+ans-psum[i])
+        cm = 0
+        for j in range(5):
+            if let[j] == c: continue
+            cv = -val[let[j]] if val[let[j]]<b else val[let[j]]
+            cm = max(cm, cv+ans-psum[j][i])
+        m.append(cm)
         cur = -v if v<b else v
         ans += cur
         b = max(b, cur)
