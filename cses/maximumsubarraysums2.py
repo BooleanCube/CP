31f76ecacd301613ac4246a418d0ecb8611e9d7e
boole <samarthupadhya2309@gmail.com>
2023-10-05 01:49:03 -0400

    greedy practice

--- cses/maximumsubarraysums2.py
+++ cses/maximumsubarraysums2.py
@@ -1,12 +1,14 @@
-n = int(input())
+n, a, b = map(int, input().split())
 ps = [0] + list(map(int, input().split()))
 for i in range(1, n): ps[i+1] += ps[i]
-m = -1e10
+m = -1e15
 lp, rp = 1, 1
 while lp <= rp <= n:
-    s = ps[rp] - ps[lp-1]
-    m = max(m, s)
-    if lp == rp: rp += 1
+    s, l = ps[rp] - ps[lp-1], rp-lp+1
+    if a <= l <= b: m = max(m, s)
+    if l < a: rp += a-l
+    elif l > b: lp += l-b
+    elif lp == rp: rp += 1
     elif s < 0: lp = rp
     elif ps[rp]-ps[lp] > s: lp += 1
     else: rp += 1
