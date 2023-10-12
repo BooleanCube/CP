6d77be6efa7f2959ba2aedffa2e74819dc0d5ee8
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2023-10-12 16:51:13 -0400

    Delete cses/cses directory

--- cses/cses/exponentiation2.py
+++
@@ -1,2 +0,0 @@
-m=10**9+7
-for s in[*open(0)][1:]:a,*c=map(int,s.split());print(pow(a,pow(*c,m-1),m))
--- cses/cses/moviefestival.py
+++
@@ -1,3 +0,0 @@
-c=1
-i=input
-print(len([c:=a for a,b in sorted(tuple(map(int,i().split()))[::-1]for _ in"."*int(i()))if b>=c]))
