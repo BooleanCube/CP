0ec9097e101f56e99d091f03bd57e122d8a60bad
BooleanCube <supadhya@porto.ptl.ucf.edu>
2024-02-14 14:03:49 -0500

    birthday party

--- kattis/birthdayparty.py
+++ kattis/birthdayparty.py
@@ -1,28 +1,35 @@
+from collections import defaultdict
+
 def dfs(m, p):
     s = []
     v = set()
-    s.append([0, set([0])])
-    while len(s) > 0:
-        x = s[-1]
-        c = x[0]
-        v = x[1]
-        s.pop(-1)
-        print(c,v,m[c])
+    s.append(0)
+    while s:
+        x = s.pop(-1)
+        c = x
+        if c in v: continue
+        v.add(c)
         for ch in m[c]:
-            if ch == 0 and len(v) == p: return False
-            elif ch in v: continue
-            s.append([ch, v.union(set([ch]))])
-    return True
+            if ch in v: continue
+            s.append(ch)
+    return len(v) == p
 
 while 1:
-    tc = input()
-    if tc == "0 0": break
-    p, c = map(int, tc.split())
-    m = {}
-    for i in range(p): m[i] = []
+    p, c = map(int, input().split())
+    if p==0 and c==0: break
+    freq = [0]*p
+    vis = set()
     for i in range(c):
         a, b = map(int, input().split())
+        if (a,b) in vis: continue
+        vis.add((a,b)); vis.add((b,a))
+        freq[a] += 1
+        freq[b] += 1
+    m = defaultdict(list)
+    for pair in vis:
+        a,b = pair
         m[a].append(b)
-        m[b].append(a)
-    print("Yes" if c==1 or dfs(m, p) else "No")
-
+    if p==1:
+        print("Yes")
+        continue
+    print("Yes" if any(freq[i] < 2 for i in range(p)) and dfs(m, p) else "No")
